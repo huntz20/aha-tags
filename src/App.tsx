90c09b8e0862ccd5b1990ac4b4a23c4f261f4eff
huntz20 <huntzrahmadi@gmail.com>
2023-01-17 23:28:50 +0700

    adding navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import './App.css'
-import Slider from "./components/Slider";
-import {useState} from "react";
+import Navbar from "./components/Navbar";
+
 
 function App() {
-    const [sliderPos, setSliderPos] = useState(2)
-    const handleSlideChange = (e) => {
-        setSliderPos(e)
-    }
     return (
         <div className="App">
-            <div className="p-4">
-                <Slider position={sliderPos} onChange={handleSlideChange}/>
-            </div>
+            <Navbar/>
         </div>
     )
 }
